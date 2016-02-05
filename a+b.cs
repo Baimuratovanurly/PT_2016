8d1398f9c3e9654d66f1d9d03cb5ccbfdd3bef41
Your Name <you@example.com>
2016-02-05 22:48:49 +0600

    a+b.cs

---
+++ a+b.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Complex
+    {
+        public double a, b;
+        public Complex (double c, double d)
+
+        {
+            a = c;
+            b = d;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex(c1.a + c2.a, c1.b + c2.b);
+            return c3;
+      
+        }
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d1 = Int32.Parse(Console.ReadLine());
+            double d2 = Int32.Parse(Console.ReadLine());
+            double h1 = Int32.Parse(Console.ReadLine());
+            double h2 = Int32.Parse(Console.ReadLine());
+            Complex d = new Complex(d1, d2);
+            Complex h = new Complex(h1, h2);
+            Complex j = d + h;
+            Console.WriteLine(j);
+            Console.ReadKey();
+        }
+    }
+}
+
